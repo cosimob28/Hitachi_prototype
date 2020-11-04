5f3435cb285b1cb8f56eaa762b01e8611f54f885
Alex <bacco.a10@gmail.com>
2020-11-04 22:56:33 +0100

    set up change station - wip

--- Assets/Scripts/ChangeStartStation/ChangeStartStation.cs
+++ Assets/Scripts/ChangeStartStation/ChangeStartStation.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class ChangeStartStation : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform train_position;
+    [SerializeField]
+    public Transform[] station_position;
+
+    public int N_STATION = 3;
+
     void Start()
+    {
+        train_position = GameObject.Find("Treno").GetComponent<Transform>();
+        station_position = new Transform[N_STATION];
+
+        for (int i = 0; i < N_STATION; i++)
+        {
+            station_position[i] = GameObject.Find("Stazione_" + i.ToString()).GetComponent<Transform>();
+        }
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void SetTrainPositionFromStation() {
+
     }
 }
