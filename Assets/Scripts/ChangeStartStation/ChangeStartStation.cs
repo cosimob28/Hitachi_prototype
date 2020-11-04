46efd6d822f433791cf471d2062bbc2ce218ca05
Alex <bacco.a10@gmail.com>
2020-11-04 23:31:58 +0100

    change station from dropdown - wip

--- Assets/Scripts/ChangeStartStation/ChangeStartStation.cs
+++ Assets/Scripts/ChangeStartStation/ChangeStartStation.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChangeStartStation : MonoBehaviour
 {
@@ -8,25 +9,35 @@ public class ChangeStartStation : MonoBehaviour
     [SerializeField]
     public Transform[] station_position;
 
+    Dropdown dropDown;
     public int N_STATION = 3;
+
+    // https://answers.unity.com/questions/1569855/search-in-dropdown-options.html
 
     void Start()
     {
         train_position = GameObject.Find("Treno").GetComponent<Transform>();
         station_position = new Transform[N_STATION];
 
-        for (int i = 0; i < N_STATION; i++)
+        dropDown = GameObject.Find("Dropdown").GetComponent<Dropdown>();
+
+        for (int i = 0; i < N_STATION+1; ++i)
         {
             station_position[i] = GameObject.Find("Stazione_" + i.ToString()).GetComponent<Transform>();
+            dropDown.value = i;
         }
     }
 
     void Update()
     {
-        
+        SetTrainPositionFromStation();
     }
 
-    void SetTrainPositionFromStation() {
-
+    public void SetTrainPositionFromStation() {
+        if (dropDown.value == 1)
+        {
+            train_position.localPosition = GameObject.Find("Stazione_3").GetComponent<Transform>().localPosition;
+        }
+        
     }
 }
