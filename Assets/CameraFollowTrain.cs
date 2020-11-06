877d9c995f592f5a996eddd03610c9a4300c1251
ftagliento <francesco.tagliento@gmail.com>
2020-11-06 17:08:24 +0100

    Modifiche Camera Follow Train

---
+++ Assets/CameraFollowTrain.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowTrain : MonoBehaviour
+{
+    public GameObject train;
+    private Vector3 cameraLocation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //calcolo della distanza tra la camera e il treno
+        cameraLocation = transform.position - train.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ad ogni frame, viene aggiornata la posizione della camera 
+        transform.position = train.transform.position + cameraLocation;
+    }
+}
